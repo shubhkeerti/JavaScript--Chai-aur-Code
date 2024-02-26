eae253931ee2a1948aad3a7448b5ca604d98e448
Shubh <124171809+shubhkeerti@users.noreply.github.com>
2024-02-26 23:01:18 +0530

    Create 01_variables.js
    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 22;
+let accountEmail = "shubhkeerti13@gmail.com";
+var acoounPassword = 5445545;
+
+// accountId = 2;
+
+
+accountCity = "Patna"
+accountEmail = "sk@sk.com"
+accountPassword = "323232"
+            
+console.log(accountCity);
+console.table([accountEmail, accountPassword])
+
+
+// do not use var because it is globally scoped 

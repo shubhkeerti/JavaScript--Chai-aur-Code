aa8da5cfbde2ffbaa9661235c174ed3e620edb50
Shubh <124171809+shubhkeerti@users.noreply.github.com>
2024-02-26 23:03:50 +0530

    Create 01_arrays.js

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,28 @@
+// Arrays
+
+const myArrays = [1, 2, 3, 4, 5, "Shubh"];
+const myHero = ["Hanuman"]
+
+// const myArr2 = new Array(1,2,3,4,5)
+// console.log(myArr2[1]);
+
+// Arrays Methods
+
+// myArrays.push(6)
+// myArrays.push("Keerti")
+// myArrays.pop()
+
+
+// myArrays.unshift(0)
+// myArrays.shift()
+
+// console.log(myArrays.includes("Shubh"));
+// console.log(myArrays.indexOf(19));
+// result will be -1
+
+console.log(myArrays);
+
+const newArr = myArrays.join
+console.log(myArrays);
+console.log(newArr);
+console.log("HellobWorld ")
